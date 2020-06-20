c95031f2d59f6318c2425db28e6bcbc53f76cc36
1071427874@qq.com <1071427874@qq.com>
2020-06-21 00:55:28 +0800

    定时任务quartz+日志serilog

---
+++ Common/LogSerilog.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using Serilog.Events;
+using System.IO;
+
+namespace Common
+{
+    public class LogSerilog
+    {
+        private static int i = 0;
+
+        /// <summary>
+        /// 应用程序第一次加载时调用
+        /// </summary>
+        public static void Init()
+        {
+            if (i==1)
+                return;
+            string p = Path.Combine("logs", @"log.txt");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(p, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            i++;
+        }
+    }
+}
---
+++ 定时任务/Task/Program.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace TaskScheduling
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            LogSerilog.Init();  //第一次加载时调用
+            await TaskQuartz.AddJob<Job1>("每隔5秒", "*/5 * * * * ?");//每隔5秒执行一次这个方法
+            Console.ReadKey();
+        }
+    }
+
+    public class Job1 : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+           return Task.Factory.StartNew(() => {
+                Console.WriteLine("执行一次");
+            });
+        }
+    }
+}
